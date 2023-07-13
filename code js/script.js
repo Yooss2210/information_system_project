44db17748f6e02ee1c726980f8d5afbb82b7c830
Carlos Jimenez <carlosmjimenezg@gmail.com>
2023-07-13 00:23:40 -0400

    Se agrego un ejemplo de DataTable

--- code js/script.js
+++ code js/script.js
@@ -1,59 +1,3 @@
-//Activo en el menu
-
-let list = document.querySelectorAll('.list');
-for (let i=0; i<list.length; i++){
-    list[i].onclick = function(){
-        let j = 0;
-        while(j < list.length){
-            list[j++].className = 'list';
-        }
-        list[i].className = 'list active'
-        
-    }
-} 
-
-//Codigo para cuando el usuario elija el estado le muestre los municipios de ese estado
-const estadosMunicipios = {
-    "Nueva Esparta": ["Arismendi", "Diaz", "Garcia", "Gomez", "Maneiro", "Marcano", "Mariño", "Peninsula de Macanao", "Tubores", "Villalba"]
-}
-
-    const inputEstados = document.querySelector('#input-estados');
-    const inputMunicipios = document.querySelector('#input-municipios');
-    
-    inputEstados.addEventListener('change', (event) => {
-        const estadoSeleccionado = event.target.value;
-        const municipios = estadosMunicipios[estadoSeleccionado];
-    
-        // Limpiar el contenido del input de municipios
-        inputMunicipios.innerHTML = '';
-    
-        // Agregar las opciones de municipios al input de municipios
-        for (const municipio of municipios) {
-            const option = document.createElement('option');
-            option.value = municipio;
-            option.textContent = municipio;
-            inputMunicipios.appendChild(option);
-        }
-});
-
-
-//Formulario de datos personales, dependiendo de tipo de persona elija el usuario le muestra el formulario
-function mostrarFormulario() {
-    var tipoPersona = document.getElementById("tipo-persona").value;
-    if (tipoPersona === "natural") {
-        document.getElementById("formulario-natural").style.display = "block";
-        document.getElementById("formulario-juridica").style.display = "none";
-    } else if (tipoPersona === "juridica") {
-        document.getElementById("formulario-natural").style.display = "none";
-        document.getElementById("formulario-juridica").style.display = "block";
-    } else {
-        document.getElementById("formulario-natural").style.display = "none";
-        document.getElementById("formulario-juridica").style.display = "none";
-    }
-}
-
-// DataTable
-
 let dataTable;
 let dataTableIsInitialized = false;
 
@@ -344,7 +288,7 @@ const listUsers = async () => {
 
     let content = ``;
     users.forEach((user, index) => {
-    content += `
+      content += `
                 <tr>
                     <td> ${index + 1} </td>
                     <td> ${user.name} </td>
